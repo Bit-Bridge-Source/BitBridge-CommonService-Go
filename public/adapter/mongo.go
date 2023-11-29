b5324ce592891bc5a53fe02f61639205338afa11
Jonathan <48375298+JonathanDowdell@users.noreply.github.com>
2023-11-28 17:53:04 -0800

    feat(mongo.go): add MongoAdapter interface and MongoAdapterImpl struct
    This commit adds a new file `mongo.go` which contains the definition of the `MongoAdapter` interface and the implementation struct `MongoAdapterImpl`. The `MongoAdapter` interface defines the methods for interacting with a MongoDB database, such as `InsertOne`, `UpdateOne`, `DeleteOne`, and `FindOne`. The `MongoAdapterImpl` struct implements these methods by delegating the calls to the underlying `MongoAdapter` instance.
    
    This commit also includes the creation of a factory function `NewMongoAdapter` which creates a new instance of `MongoAdapterImpl` with the provided `MongoAdapter` instance.
    
    The purpose of this change is to provide a flexible and modular way to interact with a MongoDB database by abstracting the implementation details behind the `MongoAdapter` interface. This allows for easier testing and swapping out different implementations of the adapter in the future.

---
+++ public/adapter/mongo.go
@@ -0,0 +1,48 @@
+package adapter
+
+import (
+	"context"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type MongoAdapter interface {
+	InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error)
+	UpdateOne(ctx context.Context, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error)
+	DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error)
+	FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult
+}
+
+type MongoAdapterImpl struct {
+	Adapter MongoAdapter
+}
+
+// DeleteOne implements IUserMongoAdapter.
+func (m *MongoAdapterImpl) DeleteOne(ctx context.Context, filter interface{}, opts ...*options.DeleteOptions) (*mongo.DeleteResult, error) {
+	return m.Adapter.DeleteOne(ctx, filter, opts...)
+}
+
+// FindOne implements IUserMongoAdapter.
+func (m *MongoAdapterImpl) FindOne(ctx context.Context, filter interface{}, opts ...*options.FindOneOptions) *mongo.SingleResult {
+	return m.Adapter.FindOne(ctx, filter)
+}
+
+// InsertOne implements IUserMongoAdapter.
+func (m *MongoAdapterImpl) InsertOne(ctx context.Context, document interface{}, opts ...*options.InsertOneOptions) (*mongo.InsertOneResult, error) {
+	return m.Adapter.InsertOne(ctx, document, opts...)
+}
+
+// UpdateOne implements IUserMongoAdapter.
+func (m *MongoAdapterImpl) UpdateOne(ctx context.Context, filter interface{}, update interface{}, opts ...*options.UpdateOptions) (*mongo.UpdateResult, error) {
+	return m.Adapter.UpdateOne(ctx, filter, update, opts...)
+}
+
+// NewMongoAdapter creates a new UserMongoAdapter
+func NewMongoAdapter(adapter MongoAdapter) *MongoAdapterImpl {
+	return &MongoAdapterImpl{
+		Adapter: adapter,
+	}
+}
+
+var _ MongoAdapter = (*MongoAdapterImpl)(nil)
